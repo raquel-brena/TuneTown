2d7e2cbfb36e573d5be5b0fec269dec4e7a97923
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-19 21:21:42 -0300

    refactor: Update main.tsx to include routing and authentication components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Profile } from './pages/Profile.tsx';
+import { HomePage } from './pages/HomePage.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import AuthProvider from './components/route/AuthProvider.tsx';
+import { ProtectedComponent } from './components/route/ProtectedRoute.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: 
+      <ProtectedComponent>
+        <HomePage />
+      </ProtectedComponent>,
+    errorElement: <div>Not Found</div>,
+  },
+  {
+    path: "/:profileId",
+    element: <Profile />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <GoogleOAuthProvider clientId="261384658112-lkecapjtglp4l818sppi3d3695jaml9k.apps.googleusercontent.com">
+    <AuthProvider isSignedIn={false}>
+      <RouterProvider router={router} />
+    </AuthProvider>
+   </GoogleOAuthProvider>
+  </React.StrictMode>
+);
