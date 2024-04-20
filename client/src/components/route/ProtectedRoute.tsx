718103b94b55494b7f2202d8dfb279053c8d2518
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-19 21:20:23 -0300

    feat: Add ProtectedRoute component for authenticated routes

---
+++ client/src/components/route/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./UseAuth";
+
+type ProtectedComponentProps = PropsWithChildren;
+
+export const ProtectedComponent = ({ children }: ProtectedComponentProps) => {
+    const user = useAuth();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user == null){
+            navigate('/home', {replace: true})
+        } else {
+            navigate(`/${user.id}`);
+        }
+    }, [navigate, user])
+    return (
+        <>{children}</>
+    );
+};
