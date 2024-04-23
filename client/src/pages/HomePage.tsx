8a1ee7817104ade7fed5e888dd051e48cb0901d8
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-22 21:04:21 -0300

    refactor: Update HomePage component UI with new design and forms

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -19,13 +19,13 @@ export const HomePage = () => {
         blur-3xl opacity-5"
       />
       <div className="size-[100%] rounded-full blur-3xl  bg-purple-600  translate-x-[95%] translate-y-96 opacity-10 absolute" />
-      <ThemeButton />
+      <div className="absolute top-4 left-4"><ThemeButton /></div>
       <div className=" relative h-auto md:w-[30%] hidden md:flex ">
         <img className="size-fit" src={logo} alt="Logo" />
       </div>
 
       <div className="flex flex-col w-full md:w-1/4 space-y-6 p-12 md:p-0">
-        <h1 className="font-semibold text-3xl  ">Inscreva-se hoje</h1>
+        <h1 className="font-semibold  text-3xl  ">Inscreva-se hoje</h1>
         <div className="justify-center overflow-hidden">
           <GoogleLogin
             size="large"
