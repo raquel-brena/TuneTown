bff5868a1fe9a47d472a365a5bfc9a56170d998e
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-25 18:25:19 -0300

    refactor: Refactor Button and ThemeButton components to improve UI and add new features

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,23 +1,28 @@
 
 interface ButtonProps {
-    text: string;
-    full?: boolean;
+  full?: boolean;
+  onClick?: () => void;
+  bg?: string;
+  children: React.ReactNode;
 }
 
-export const Button = ({ text, full }: ButtonProps) => {
+export const Button = ({  full, bg,  onClick, children }: ButtonProps) => {
   return (
     <button
-    type = "button"
-      className={`flex items-center font-semibold justify-center text-center 
-        ${
-          full
-            ? "bg-transparent text-theme border border-theme hover:text-theme/10"
-            : "bg-theme hover:bg-fume/10 border-stroke "
-        } 
-        hover:opacity-90 transition-opacity 
-        w-full py-2 rounded-sm text-base`}
+      type="button"
+      onClick={onClick}
+      className={`flex items-center border font-semibold justify-center text-center 
+    ${
+      full
+        ? "bg-transparent text-theme border border-theme hover:text-theme/10"
+        : bg
+        ? `${bg} hover:bg-fume/10 border  border-stroke`
+        : `bg-theme hover:bg-fume/10 border border-stroke`
+    } 
+    hover:opacity-90 transition-opacity 
+    w-full py-2 rounded-sm text-base`}
     >
-      {text}
+      {children}
     </button>
   );
 };
