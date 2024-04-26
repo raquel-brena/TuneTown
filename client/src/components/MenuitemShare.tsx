04a01a7e5b46a05a5827d7628a4b6adb3ed9f792
raquel-brena <raquel.lima.124@ufrn.edu.br>
2024-04-26 18:54:39 -0300

    feat: Add MenuitemShare component to client/src/components/MenuitemShare.tsx

---
+++ client/src/components/MenuitemShare.tsx
@@ -0,0 +1,6 @@
+export const MenuitemShare = () => {
+
+    return (
+        <div className="h-4 w-fit py-4 px-8 text-center items-center justify-center flex rounded-full border border-stroke" >Musica</div>
+    )
+}
