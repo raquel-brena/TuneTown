a607b79e629e9d5f81783558a79b13ba83a56a8c
raquel-brena <raquel.lima.124@ufrn.edu.br>
2024-04-26 18:54:55 -0300

    feat: Add Share component to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,20 @@
 import { useParams } from "react-router-dom";
 import { Menu } from "../components/Menu.tsx";
 import { ThemeButton } from "../components/ThemeButton.tsx";
+import { Share } from "../components/Share.tsx";
+import { Container } from "../components/Container.tsx"
 export const Home = () => {
 
     return (
         <div className="bg-base text-contrast items-center flex fixed w-screen h-screen text-balance">
-            <div className="absolute top-4 z-40 left-4">
-                <ThemeButton/>
+            <div className="absolute hidden md:flex md:top-4 md:z-40 md:left-4">
+                <ThemeButton />
             </div>
             <Menu />
+            <Container>
+                <Share />
+            </Container>
+
         </div>
     );
 
