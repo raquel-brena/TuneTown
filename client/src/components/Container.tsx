21a511a07e81c9cf34591fc162fed287c865ac4e
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-30 14:01:15 -0300

    fix: Refactor Container component to  responsive props

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,7 +1,7 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
 
     return (
-        <div className="h-full space-y-10 mt-12 w-full md:w-[60%] flex relative justify-content items-center flex-col">
+        <div className="h-full space-y-10 mx-2 md:mx-0 mt-12 w-full md:w-[60%] flex relative justify-content items-center flex-col">
             {children}
         </div>
     )
