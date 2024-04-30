3c27639fc7f56461bf391bb6f5d4bc26b0237560
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-30 14:00:02 -0300

    fix: Refactor Photo component to include size and shape properties

--- client/src/components/Photo.tsx
+++ client/src/components/Photo.tsx
@@ -1,6 +1,6 @@
 export const Photo = () => {
 
     return (
-        <div className="size-12 rounded-full bg-[#D9D9D9]" />
+        <div className="h-12 w-12 flex-none rounded-full bg-[#D9D9D9]" />
     )
 }
