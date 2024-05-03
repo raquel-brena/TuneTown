7df2d519946a6b1cb9e860398132dd6a6bcafc08
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-03 09:07:32 -0300

    chore: Add new SVG icons for left menu and top menu

---
+++ client/src/assets/left_menu/index.ts
@@ -0,0 +1,7 @@
+// in left_menu/index.ts
+export { default as config } from "./config.svg";
+export { default as foruns } from "./foruns.svg";
+export { default as home } from "./home.svg";
+export { default as more } from "./more.svg";
+export { default as profile } from "./profile.svg";
+export { default as search } from "./search.svg";
