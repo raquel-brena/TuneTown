4e616a8e56d69f0b87319268d850d8252bcd208c
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-23 12:18:43 -0300

    refactor: Update Input component styles

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -14,8 +14,8 @@ function Input(props: InputProps) {
            sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] 
             bg-transparent ring-1 ring-white/30 placeholder:text-zinc-500 sm:text-sm/6 
             text-base/6
-             block
-            focus:ring-0
+            block
+            focus:ring-0 
             hover:ring-white/40
             after:inset-0 after:rounded-lg
            "
