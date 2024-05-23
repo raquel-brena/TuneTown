d9a9e5599fd1948a0afcb982b5a462502a6a093a
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-23 12:17:47 -0300

    refactor: Update Menu import in Feed.tsx to use MenuLeft component

--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,21 +1,18 @@
 
-import { Menu } from "../components/MenuLeft.tsx";
+import { MenuLeft } from "../components/feed/menuLeft/MenuLeft.tsx";
 import { ThemeButton } from "../components/ThemeButton.tsx";
-import { Share } from "../components/MenuTop.tsx";
-import { Container } from "../components/Container.tsx"
+import { MenuTop } from "../components/feed/menuTop/MenuTop.tsx";
+import { Container } from "../components/feed/Container.tsx"
 
 export const Home = () => {
     return (
-        <div className="bg-base text-contrast items-center flex fixed w-screen h-screen text-balance">
-            <div className="absolute hidden md:flex md:top-4 md:z-40 md:left-4">
-                <ThemeButton />
-            </div>
-            <Menu />
-            <Container>
-                <Share />
-            </Container>
-
+      <div className="bg-base text-contrast items-center flex fixed w-screen h-screen text-balance">
+        <div className="absolute hidden md:flex md:top-4 md:z-40 md:left-4">
+          <ThemeButton />
         </div>
+        <MenuLeft />
+        <Container/>
+      </div>
     );
 
 };
