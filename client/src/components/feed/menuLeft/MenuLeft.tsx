c84bad2b4f841a39d21cf3a2d30c30329922d762
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-26 22:03:43 -0300

    refactor: Update MenuLeft component to use black theme SVG icons

--- client/src/components/feed/menuLeft/MenuLeft.tsx
+++ client/src/components/feed/menuLeft/MenuLeft.tsx
@@ -1,37 +1,74 @@
-import { MenuLeftItem } from "./MenuLeftItem"
-import {  profile, foruns, home, search}  from "../../../assets/left_menu";
-import { Img } from "../../Img"
+import { MenuLeftItem } from "./MenuLeftItem";
+import {
+  home,
+  home2,
+  search,
+  search2,
+  profile,
+  profile2,
+  foruns,
+  foruns2,
+  more, more2,
+  config,
+  config2
+} from "../../../assets/left_menu";
+import { useEffect, useState } from "react";
 
-export const MenuLeft = () => {
+type MenuLeftProps = {
+  buttonSelected: string;
+  setButtonSelected: (value: string) => void;
+};
 
-    return (
-      <div
-        className="md:h-full md:w-1/12 
+
+export const MenuLeft = ({
+  buttonSelected,
+  setButtonSelected,
+}: MenuLeftProps) => {
+  const [theme, setTheme] = useState<string>("light");
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme")!);
+  }, []);
+
+  const items = {
+    home: { src: theme === "light" ? home2 : home, alt: "home" },
+    search: { src: theme === "light" ? search2 : search, alt: "search" },
+    profile: { src: theme === "light" ? profile2 : profile, alt: "profile" },
+    foruns: { src: theme === "light" ? foruns2 : foruns, alt: "foruns" },
+    more: { src: theme === "light" ? more : more2, alt: "more" },
+  };
+
+  return (
+    <div
+      className="md:h-full md:w-1/12 
       justify-center items-center flex 
       h-12 w-full absolute bottom-0 border-box 
-      border border-stroke md:relative overflow-hidden"
+      border border-stroke md:relative overflow-hidden md:z-0 z-30"
+    >
+      <div
+        className="h-2/3 w-full justify-between sm:flex-row text-contrast gap-4 
+      md:flex-col items-center flex md:bg-transparent bg-fume"
       >
-        <div className="h-2/3 w-full sm:flex-row text-contrast gap-4 md:flex-col items-center flex">
-          <MenuLeftItem>
-            <Img src={home} alt="Logo" />
-          </MenuLeftItem>
-          
-          <MenuLeftItem>
-            <Img src={search} alt="Logo" />
-          </MenuLeftItem>
-
-          <MenuLeftItem>
-            <Img src={foruns} alt="Logo" />
-          </MenuLeftItem>
-
-          <MenuLeftItem>
-            <Img src={profile} alt="Logo" />
-          </MenuLeftItem>
-        </div>
-
         <div
-          className={`absolute h-1/2 w-48 bg-copacity_theme rotate-90 blur-3xl translate-y-96 `}
-        />
+          className="h-[80%] flex md:flex-col md:bg-transparent 
+        flex-row bg-fume w-full sh-full gap-4 items-center"
+        >
+          {Object.values(items).map((item, index) => (
+            <MenuLeftItem
+              key={index}
+              src={item.src}
+              alt={item.alt}
+              buttonSelected={buttonSelected}
+              setButtonSelected={setButtonSelected}
+            />
+          ))}
+        </div>
+        <MenuLeftItem src={config} alt={"config"} />
       </div>
-    );
-}
+
+      <div
+        className={`absolute h-1/2 w-48 bg-copacity_theme rotate-90 blur-3xl translate-y-96 `}
+      />
+    </div>
+  );
+};
--- client/src/components/feed/menuLeft/MenuLeftItem.tsx
+++ client/src/components/feed/menuLeft/MenuLeftItem.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import { Img } from "../../Img";
 
-export const MenuLeftItem = ({ children }: { children: React.ReactNode }) => {
+interface MenuLeftItemProps {
+  src: string;
+  alt: string;
+  buttonSelected: string;
+  setButtonSelected: (value: string) => void;
+}
+
+export const MenuLeftItem = ({ src, alt, buttonSelected, setButtonSelected}: MenuLeftItemProps) => {
+  
+  function selectMenu(alt: string) {
+    setButtonSelected(alt);
+  }
+
   return (
-    <button className="flex  h-12 p-6 w-full items-center justify-center group ">
-      {children}
+    <button
+      onClick={() => selectMenu(alt)}
+      className="flex  h-10 p-6 fill-contrast stroke-contrastw-full items-center justify-center group
+    "
+    >
+      <Img src={src} alt={alt} />
       <div
-        className="transition-all group-hover:absolute group-hover:size-12 group-hover:bg-copacity_25 
-      group-hover:rounded-full"
-      >
-      </div>
+        className={`${
+          alt !== "more"
+            ? alt === buttonSelected
+              ? `absolute size-12 bg-copacity_25 rounded-full`
+              : "transition-all group-hover:absolute group-hover:size-12 group-hover:bg-copacity_25 group-hover:rounded-full"
+            : `md:-z-10  absolute size-12 bg-theme rounded-full hover:bg-copacity_theme/5 transition-all`
+        }`}
+      ></div>
     </button>
   );
 };
