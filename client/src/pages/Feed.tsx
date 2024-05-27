253bede115e511a2111053917d6210157a067919
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-27 01:57:49 -0300

    refactor: Remove unnecessary CSS styling from Feed page

--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,8 +1,6 @@
 import Card from "../components/feed/container/cards/Card";
 import { MenuTop } from "../components/feed/container/menuTop/MenuTop";
 import { ContainerCard } from "../components/feed/container/ContainerCard";
-import { ContainerCentral } from "./ContainerCentral";
-
 
 export const Feed = () => {
 
@@ -11,7 +9,7 @@ export const Feed = () => {
  const longText =
    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, ";
     return (
-      <div className="justify-content mt-4 flex flex-col items-center ">
+      <div className="justify-content mt-4 flex flex-col items-center">
         <MenuTop />
         <ContainerCard>
           <Card title="title" created_at="08 ago" content={text1} />
