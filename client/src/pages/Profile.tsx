99ee4768a89c6530d64e05d09779f6a9214a885d
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-27 00:43:13 -0300

    refactor: Update Profile page layout and styling

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,13 +1,19 @@
 import { useParams } from "react-router-dom";
+import { ContainerCentral } from "./ContainerCentral";
 
 export const Profile = () => {
     const { profileId } = useParams<{profileId: string}>();
 
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>{profileId}</p>
-        </div>
+      <div
+        className="h-full md:w-8/12 w-full rounded-lg border-box relative bg-fume 
+      border border-stroke overflow-hidden"
+      >
+        <>
+          <h1>Profile</h1>
+          <p>{profileId}</p>
+        </>
+      </div>
     );
     
 };
