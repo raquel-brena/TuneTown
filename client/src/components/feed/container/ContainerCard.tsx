b2c5628316f01cd73c84f385ce6a4eef3fd69222
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-27 00:22:23 -0300

    refactor: Remove unnecessary CSS styling from ContainerCard component

--- client/src/components/feed/container/ContainerCard.tsx
+++ client/src/components/feed/container/ContainerCard.tsx
@@ -4,7 +4,7 @@ export const ContainerCard = ({children}: {children: React.ReactNode}) => {
 
     return (
         <div className="flex flex-col gap-3 w-full h-[85%] items-center 
-        overflow-y-auto scroll-smooth scroll snap-start mt-6 snap-y">
+       snap-start mt-6 snap-y">
         {children}
         </div>
     )
