066d73ee0b06913d586057946e2655a42c45597d
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-30 17:17:22 -0300

    feat:  sign-up form layout and components

--- client/src/app/components/forms/FormSignUp.tsx
+++ client/src/app/components/forms/FormSignUp.tsx
@@ -1,8 +1,27 @@
-
+import { Button } from "../Button";
+import Input from "../Input";
 
 export const FormSignUp = () => {
+
+
     return (
-      <div></div>
+      <div className="flex flex-col space-y-5 px-6 py-5">
+        <div className="mb-4">
+          <p className="text-3xl mb-1 font-semibold">Cadastre-se!</p>
+          <p className="text-md font-light">
+            Esteja conectado aos seus amigos e suas músicas
+          </p>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          <Input label="Nome" />
+          <Input label="Email" />
+          <Input label="Username" />
+          <Input label="Senha" />
+        </div>
+
+        <Button>Criar conta</Button>
+      </div>
     );
   };
   
