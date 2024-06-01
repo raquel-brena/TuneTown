9aed4043673e528b3646139408221baba4513b28
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-01 13:57:45 -0300

    refactor: Refactor Input component to use forwardRef and update prop types

--- client/src/app/components/Input.tsx
+++ client/src/app/components/Input.tsx
@@ -1,15 +1,16 @@
-interface InputProps {
-    label: string
+import React, { forwardRef, useRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
 }
 
-function Input(props: InputProps) {
-    return (
-      <div className="flex flex-col space-y-2">
-        <label className="text-sm font-semibold select-none">
-          {props.label}
-        </label>
-        <input
-          className="rounded-sm h-8 appearance-none bg-slate-200 relative
+const Input = forwardRef<HTMLInputElement, InputProps>(({ label, ...props }, ref) => {
+  return (
+    <div className="flex flex-col space-y-">
+      <label className="text-sm font-light select-none">{label}</label>
+      <input
+        ref={ref}
+        className="rounded-sm h-8 appearance-none bg-slate-200 relative
            px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] 
            sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] 
             bg-transparent ring-1 ring-white/30 placeholder:text-zinc-500 sm:text-sm/6 
@@ -19,10 +20,10 @@ function Input(props: InputProps) {
             hover:ring-white/40
             after:inset-0 after:rounded-lg
            "
-          name="full_name"
-        />
-      </div>
-    );
-}
+        {...props}
+      />
+    </div>
+  );
+});
 
 export default Input;
