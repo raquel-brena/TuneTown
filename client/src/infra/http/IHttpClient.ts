0391c5561254b53feac7cc4bc3ebad1a3cf43d61
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-31 21:01:59 -0300

    refactor: Remove unnecessary server files, routes, and database schema

--- client/src/infra/http/IHttpClient.ts
+++ client/src/infra/http/IHttpClient.ts
@@ -1,4 +1,4 @@
-export default interface HttpClient {
+export default interface IHttpClient {
     get (url: string):Promise <any>;
     post (url: string, data: any):Promise <any>;
     put (url: string, data: any):Promise <any>;
