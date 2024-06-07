e03fb8e0eba7780ec540c9b4f1eba86543e94ce2
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-06 23:16:05 -0300

    refactor: Update server app.ts to include routes for user authentication and error handling

---
+++ server/app.ts
@@ -0,0 +1,42 @@
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+
+import indexRouter from "./src/app/routes/index";
+import userRoutes from "./src/app/routes/users";
+import authRoutes from "./src/app/routes/auth";
+
+const app = express();
+
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+
+
+// catch 404 and forward to error handler
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server running on port ${port}`));
