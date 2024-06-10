3524c9305f81474e91cd773cdc54c1191993b2b3
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-10 19:58:14 -0300

    refactor: Add API service for making HTTP requests

---
+++ client/src/app/services/api/index.tsx
@@ -0,0 +1,38 @@
+import { getToken, refreshAccessToken } from "./token";
+
+const baseURL = "http://localhost:8000/"
+
+const api = async (url: string, options: RequestInit = {}): Promise<any> => {
+  try {
+    const token = getToken();
+    const defaultOptions: RequestInit = {
+      method: options.method || "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : "",
+        ...options.headers,
+      },
+      ...(options.body && { body: options.body }),
+    };
+
+    const response = await fetch(`${baseURL}${url}`, defaultOptions);
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (response.status === 401 && !url.startsWith("/authenticate/login/")) {
+        await refreshAccessToken();
+        getToken();
+        return api(url, options);
+      }
+
+      throw new Error(`${errorData.error}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error while making the request:", error);
+    throw error;
+  }
+};
+
+export { api, baseURL };
