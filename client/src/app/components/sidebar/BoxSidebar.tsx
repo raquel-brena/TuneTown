8c126ef31a8be715b4140b3988c040cb152604a2
Raquel <raquel.lima.124@ufrn.edu.br>
2024-06-16 15:15:51 -0300

    chore: remove client repository

--- client/src/app/components/sidebar/BoxSidebar.tsx
+++
@@ -1,8 +0,0 @@
-export const BoxSidebar = () => {
-
-
-    return (
-        <div className="flex flex-col h-[50%] rounded-md w-full bg-fume border border-stroke ">
-            a
-        </div>
-    ) }
--- client/src/app/services/auth/signInRequest.ts
+++
@@ -1,8 +0,0 @@
-import { UserLoginDTO } from "../../../domain/types/Auth";
-import AuthGatewayHttp from "../../../infra/gateway/AuthGatewayHttp";
-
-export async function signInRequest(userData: UserLoginDTO) {
-    const authGatewayHttp = new AuthGatewayHttp();
-    const response = await authGatewayHttp.signIn(userData);
-    return response;
-}
